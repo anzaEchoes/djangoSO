2b9c1567dd8d9aff4f169b2814db41bcddb75297
DavidMarcelo <cdmarceloz@gmail.com>
2019-12-01 14:38:44 -0600

    v2.1

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -77,14 +77,15 @@ WSGI_APPLICATION = 'finalproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-"""DATABASES = {
+"""
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'databasepg',
@@ -93,7 +94,7 @@ DATABASES = {
         'HOST': 'databasepg.cxbnferhevwz.us-east-1.rds.amazonaws.com',
         'PORT': '5432',
     }
-}"""
+}
 
 CORS_ALLOW_METHODS = (
     'DELETE',
